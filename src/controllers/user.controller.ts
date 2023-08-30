9cf7150453977d23016c88693b897c36d64167ed
SebastianOG509 <seortizg@unal.edu.co>
2023-08-29 22:48:10 -0500

    crud inicial

---
+++ src/controllers/user.controller.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import User from '../models/User';
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { username, email, password } = req.body;
+    const encryptedPassword = await User.encryptPassword(password);
+    const newUser = new User({ username, email, password: encryptedPassword });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al crear el usuario' });
+  }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener el usuario' });
+  }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+  try {
+    const { username, email } = req.body;
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      { username, email },
+      { new: true }
+    );
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: 'Error al actualizar el usuario' });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    await User.findByIdAndDelete(req.params.userId);
+    res.json({ message: 'Usuario eliminado exitosamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al eliminar el usuario' });
+  }
+};

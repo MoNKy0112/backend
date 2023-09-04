f039ea7ba16781dc8ce5f8fc41ff2865fde4a067
Fhchaparroa <fhchaparroa@gmail.com>
2023-09-03 21:26:23 -0500

    CRUD Productos

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
 export default {
-	MONGO_DATABASE: 'test2',
-	MONGO_USER: 'TiUN2023',
-	MONGO_PASSWORD: 'TiendaUNAL2023',
-	MONGO_HOST: '127.0.0.1:27017',
-	SERVER_PORT: process.env.PORT ?? 3000,
-};
+    MONGO_DATABASE: 'test2',
+    MONGO_USER: 'TiUN2023',
+    MONGO_PASSWORD: 'TiendaUNAL2023',
+    MONGO_HOST: '127.0.0.1:27017',
+    SERVER_PORT: process.env.PORT ?? 3000,
+}

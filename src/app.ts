c1d971d70f0230cbf289c7a71d40f0047d035f84
MoNKy0112 <jomoncaleanoa@unal.edu.co>
2023-10-17 02:16:56 -0500

    #

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,7 @@ import categoriesRoutes from './routes/category';
 // Settings
 app.set('port', config.SERVER_PORT || 3000);
 // Midllewares
+
 app.use(cors({
 	origin: 'https://ti-un-front-fork.vercel.app',
 	credentials: true,

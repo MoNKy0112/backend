edbdf038854ed1c9011c5b12962dfa617e4afd13
Fhchaparroa <fhchaparroa@gmail.com>
2023-10-18 23:02:34 -0500

    Recomendacion v2 :c

--- src/controllers/recommend.controller.ts
+++ src/controllers/recommend.controller.ts
@@ -1,70 +1,22 @@
-import type mongoose from 'mongoose';
-import {type IUser} from '../models/User';
-import {type IProduct} from '../models/Product';
+import Product from '../models/Product';
+import {type Request, type Response} from 'express';
 
-class RecommendSystem {
-	constructor(private readonly userModel: mongoose.Model<IUser>, private readonly productModel: mongoose.Model<IProduct>) { }
+// Controlador para obtener recomendaciones de productos
+export const getRecommendations = async (req: Request, res: Response) => {
+	try {
+		console.log(req.userId);
+		const {favoriteCategories, favoriteProducts} = req.userId;
 
-	async recommendProducts(userId: mongoose.Types.ObjectId, cantidad: number): Promise<IProduct[]> {
-		// eslint-disable-next-line no-useless-catch
-		try {
-			const usuario = await this.userModel
-				.findById(userId)
-				.populate('viewedProducts')
-				.populate('favouriteProducts')
-				.populate('orders');
+		// Buscar productos que tengan categorías en común con las categorías favoritas del usuario y que no estén en su lista de productos favoritos
+		const recommendations = await Product.find({
+			categories: {$in: favoriteCategories},
+			_id: {$nin: favoriteProducts},
+		}).limit(10); // Puedes ajustar el número de recomendaciones según tus necesidades
 
-			if (!usuario) {
-				const productosRecomendados = await this.productModel.find().limit(cantidad);
-				return productosRecomendados;
-			}
-
-			const productScores = new Map<IProduct, number>();
-
-			for (const productId of usuario.viewedProducts) {
-				// eslint-disable-next-line no-await-in-loop
-				const productInfo = await this.productModel.findById(productId);
-				if (productInfo) {
-					if (!productScores.has(productInfo)) {
-						productScores.set(productInfo, 0);
-					}
-
-					productScores.set(productInfo, productScores.get(productInfo)! + 1);
-				}
-			}
-
-			for (const productId of usuario.favoriteProducts) {
-				// eslint-disable-next-line no-await-in-loop
-				const productInfo = await this.productModel.findById(productId);
-				if (productInfo) {
-					if (!productScores.has(productInfo)) {
-						productScores.set(productInfo, 0);
-					}
-
-					productScores.set(productInfo, productScores.get(productInfo)! + 3);
-				}
-			}
-
-			for (const order of usuario.orders) {
-				for (const product of order.products) {
-					// eslint-disable-next-line no-await-in-loop
-					const productInfo = await this.productModel.findById(product.productId);
-
-					if (productInfo) {
-						if (!productScores.has(productInfo)) {
-							productScores.set(productInfo, 0);
-						}
-
-						productScores.set(productInfo, productScores.get(productInfo)! + 5);
-					}
-				}
-			}
-
-			const sortedProducts = [...productScores.entries()].sort((a, b) => b[1] - a[1]);
-
-			return sortedProducts.slice(0, cantidad).map(entry => entry[0]);
-		} catch (error) {
-			throw error;
-		}
+		return res.status(200).json({recommendations});
+	} catch (error) {
+		return res.status(500).json({error: 'Error al obtener recomendaciones de productos'});
 	}
-}
+};
+
+export default {getRecommendations};
---
+++ src/routes/recommend.ts
@@ -0,0 +1,7 @@
+import express from 'express';
+import {getRecommendations} from '../controllers/recommend.controller';
+import {tokenValidation} from '../middlewares/validateToken';
+
+const router = express.Router();
+
+router.get('/recommend', tokenValidation, getRecommendations);

14ca68c95ff62efdfc6914fdf5247a1f69bed355
MoNKy0112 <jomoncaleanoa@unal.edu.co>
2023-11-25 16:22:22 -0500

    feat middleware seller: seller token mercado pago Not yet incorporated

---
+++ src/middlewares/verifySellerToken.ts
@@ -0,0 +1,20 @@
+import {type Request, type Response, type NextFunction} from 'express';
+import authFacade from 'facades/auth.facade';
+
+export const verifyTokenMp = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const {userId} = req;
+		const user = await authFacade.getuser(userId);
+
+		if (!user.accessTokenMp) throw new Error('Necesitas vincular tu cuenta de mercadopago');
+
+		next();
+	} catch (error) {
+		if (error instanceof Error) {
+			res.status(401).json();
+		} else {
+			console.error('unknown error:', error);
+			res.status(500).json('Unknown error');
+		}
+	}
+};
